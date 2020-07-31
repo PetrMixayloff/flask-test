ba97259992106273289dc36d5f5af09e1b7205ee
Petr <p>
2020-07-31 13:25:48 +0300

    migrate

--- migrations/env.py
+++ migrations/env.py
@@ -21,6 +21,8 @@ logger = logging.getLogger('alembic.env')
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
+from api.models import Task
 from flask import current_app
 config.set_main_option(
     'sqlalchemy.url',
